5a1bd55bf66c340d2416439292c246afc9a76807
Marina <mfuster@itba.edu.ar>
2019-05-11 06:17:54 -0300

    Shared memory syscalls

--- Kernel/syscalls.c
+++ Kernel/syscalls.c
@@ -17,7 +17,7 @@
 #define ENTER '\n'
 #define DELETE '\b'
 
-#define SYSCALLSQTY 15
+#define SYSCALLSQTY 18
 #define VALID_SYS_CODE(c) (c>=0 && c<=SYSCALLSQTY)
 
 typedef uint64_t (*syscall) (uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8, uint64_t r9);
@@ -219,6 +219,9 @@ void loadSysCalls(){
   syscalls[12]=&sys_exec;
   syscalls[13]=&sys_exit_process;
   syscalls[14]=&sys_ps;
+  syscalls[15]=&sys_shm_create;
+  syscalls[16]=&sys_shm_open;
+  syscalls[17]=&sys_shm_close;
 }
 
 void sysCallsHandler(uint64_t syscode, uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8, uint64_t r9){ // lega en rdi desde asm
